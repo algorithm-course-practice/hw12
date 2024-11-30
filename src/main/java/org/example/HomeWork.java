f6fcc5f3b6aa5cf20e0d46e4072b1ac581e52e9f
Almaz <almazgara@gmail.com>
2024-11-30 11:56:56 +0300

    hw12

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,8 +3,9 @@ package org.example;
 
 import lombok.SneakyThrows;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HomeWork {
 
@@ -14,8 +15,36 @@ public class HomeWork {
      */
     @SneakyThrows
     public void frogSteps(InputStream in, OutputStream out) {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))){
+            List<String> params = reader.lines().collect(Collectors.toList());
+            List<String> input =reader.lines().collect(Collectors.toList());
+            int t = Integer.parseInt(params.get(0));
+            for (int i = 1; i <= t; i++) {
+                char[] line = params.get(i).toCharArray();
+                boolean containL = params.get(i).contains("L");
+                // находим максимальную непрерывную последовательность L чтобы лягушка могла ее перепрыгнуть
+                int maxL = countL(line, containL);
+                writer.write(maxL + 1 + "\n");
+            }
+            writer.flush();
+        }
     }
-
-
-
+    private int countL(char[] line, boolean containL){
+        int max = 0;
+        int count = containL ? 1 : 0;
+        int len = line.length;
+        for (int i = 1; i < len; i++) {
+            if (line[i -1] == 'L'){
+                if (line[i - 1] == line[i]){
+                    count++;
+                }
+                else {
+                    max = Math.max(max, count);
+                    count = containL ? 1 : 0;
+                }
+            }
+        }
+        return Math.max(max, count);
+    }
 }
